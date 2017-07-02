590bf81658f237ac7c0326a0247916ff94e1eea4
Darthpwner <matthewallenlin@gmail.com>
2017-07-02 10:04:04 -0700

    Rotator appears to work slightly

---
+++ js/infinite-rotator.js
@@ -0,0 +1,40 @@
+$(window).load(function() { //start after HTML, images have loaded
+ 
+    var InfiniteRotator = {
+        init: function() {
+            
+            //initial fade-in time (in milliseconds)
+            var initialFadeIn = 1000;
+ 
+            //interval between items (in milliseconds)
+            var itemInterval = 5000;
+ 
+            //cross-fade time (in milliseconds)
+            var fadeTime = 2500;
+ 
+            //count number of items
+            var numberOfItems = $('.rotating-photo').length;
+ 
+            //set current item
+            var currentItem = 0;
+ 
+            //show first item
+            $('.rotating-photo').eq(currentItem).fadeIn(initialFadeIn);
+ 
+            //loop through the items
+            var infiniteLoop = setInterval(function(){
+                $('.rotating-photo').eq(currentItem).fadeOut(fadeTime);
+ 
+                if(currentItem == numberOfItems -1){
+                    currentItem = 0;
+                }else{
+                    currentItem++;
+                }
+                $('.rotating-photo').eq(currentItem).fadeIn(fadeTime);
+ 
+            }, itemInterval);
+        }
+    };
+ 
+    InfiniteRotator.init();
+});
